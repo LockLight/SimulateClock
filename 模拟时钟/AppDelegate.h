678c78a4d0370c72e807d304a90c5e27732f69d2
LockLight <Locklightz@gmail.com>
2017-02-14 18:35:35 +0800

    Initial Commit

---
+++ 模拟时钟/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  模拟时钟
+//
+//  Created by locklight on 17/2/14.
+//  Copyright © 2017年 LockLight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 模拟时钟/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  模拟时钟
+//
+//  Created by locklight on 17/2/14.
+//  Copyright © 2017年 LockLight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
